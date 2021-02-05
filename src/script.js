c0ffe604481732993d6040b0cb4595e15c4ee660
PolozkovDmitriu <polozkovdmitriu@gmail.com>
2021-02-05 19:11:31 +0300

    04.02.2021 23:34 Commit five

--- src/script.js
+++ src/script.js
@@ -1,26 +1,4 @@
 
-
-
-let Key = document.getElementById('Keys').childNodes;
-// С помощью let Key = document.getElementById('Keys').childNodes.length;
-// отфильтровать блоки с текстом через цикл for
-// Полученным div применить класс css
-
-
-for (i = 0; i < 32; i++) {
-	if (i % Key.length === 0) {
-		//Применить Key.length[n]; Стиль css .keys-ops
-	}
-	if (i % Key.length !== 0) {
-		//Добовить тег <p>N</p> 
-	}
-}
-
-
-
-
-//РАсскидать блоки по полю 
-
 let key1 = document.getElementById('key1');
 let key2 = document.getElementById('key2');
 let key3 = document.getElementById('key3');
@@ -35,9 +13,83 @@ let key11 = document.getElementById('key11');
 let key12 = document.getElementById('key12');
 let key13 = document.getElementById('key13');
 let key14 = document.getElementById('key14');
+let key15 = document.getElementById('key15');
 let key0 = document.getElementById('key0');
 
 
+key1.className = 'keys-ops';
+key2.className = 'keys-ops';
+key3.className = 'keys-ops';
+key4.className = 'keys-ops';
+key5.className = 'keys-ops';
+key6.className = 'keys-ops';
+key7.className = 'keys-ops';
+key8.className = 'keys-ops';
+key9.className = 'keys-ops';
+key10.className = 'keys-ops';
+key11.className = 'keys-ops';
+key12.className = 'keys-ops';
+key13.className = 'keys-ops';
+key14.className = 'keys-ops';
+key15.className = 'keys-ops';
+key0.className = 'white-key';
+
+let key1p = document.getElementById('key-1-p');
+let key2p = document.getElementById('key-2-p');
+let key3p = document.getElementById('key-3-p');
+let key4p = document.getElementById('key-4-p');
+let key5p = document.getElementById('key-5-p');
+let key6p = document.getElementById('key-6-p');
+let key7p = document.getElementById('key-7-p');
+let key8p = document.getElementById('key-8-p');
+let key9p = document.getElementById('key-9-p');
+let key10p = document.getElementById('key-10-p');
+let key11p = document.getElementById('key-11-p');
+let key12p = document.getElementById('key-12-p');
+let key13p = document.getElementById('key-13-p');
+let key14p = document.getElementById('key-14-p');
+let key15p = document.getElementById('key-15-p');
+let key0p = document.getElementById('key-0-p');
+
+key1p.className = 'keys-p';
+key2p.className = 'keys-p';
+key3p.className = 'keys-p';
+key4p.className = 'keys-p';
+key5p.className = 'keys-p';
+key6p.className = 'keys-p';
+key7p.className = 'keys-p';
+key8p.className = 'keys-p';
+key9p.className = 'keys-p';
+key10p.className = 'keys-p';
+key11p.className = 'keys-p';
+key12p.className = 'keys-p';
+key13p.className = 'keys-p';
+key14p.className = 'keys-p';
+key15p.className = 'keys-p';
+key0p.className = 'keys-p';
+
+TestOne = function () {
+
+}
+// 1 Сделать случайное выпадение чисел на поле
+// 2 Создать функцию которая при клике:
+// 2.1 Меняет число с Белым блоком
+// 2.2 Меняет Цвет с белым блоком
+// 3 Усключить клики по блокам не имеющим право меняться с белым блоком
+// https://www.youtube.com/watch?v=lvsUEL8yqSs
+// Подумать на счет невидимости https://www.cyberforum.ru/javascript/thread630123.html
+
+
+//RandomNumbers
+
+
+
+key12.onclick = function () {
+	//Сначала удали прежний класс
+	key12p.className = 'white-key';
+	key0p.innerHTML = '12';
+	key0p.className = 'keys-ops keys-p';
+}
 //Сделать блоки кликабельными 
 
 let BLANK = [
@@ -48,19 +100,48 @@ let BLANK = [
 ];
 
 
+let BLANK_P = [
+	[1, 2, 3, 4],
+	[5, 6, 7, 8],
+	[9, 10, 11, 12],
+	[13, 14, 15, 16]
+];
 
-//children – коллекция детей, которые являются элементами.
-//firstElementChild, lastElementChild – первый и последний дочерний элемент.
-//previousElementSibling, nextElementSibling – соседи-элементы.
-//parentElement – родитель-элемент.
-//alert(document.body.childNodes.filter); // undefined (у коллекции нет метода filter!)
 
 
-//querySelector	CSS-selector	✔	-
-//querySelectorAll	CSS-selector	✔	-
-//getElementById	id	-	-
-//getElementsByName	name	-	✔
-//getElementsByTagName	tag or '*'	✔	✔
-//getElementsByClassName	class	✔	✔
+function RandomNambers() {
+	nam_a = JSON.stringify();
+	nam_b = JSON.stringify();
+	for (let i = 0; i < BLANK_P.length; i++) {
+		for (let j = 0; j < BLANK_P[i].length; j++) {
 
+			num = Math.floor(1 + Math.random() * (16 - 1));
+			BLANK_P[i][j] = num;
+
+		}
+	}
+
+
+	for (let a = 0; a < BLANK_P.length; a++) {
+		if (a == 0) {
+			for (d = 0; d < BLANK_P.length; d++) {
+				for (s = 0; s < BLANK_P.length; s++) {
+					num = Math.floor(1 + Math.random() * (16 - 1));
+					if ((BLANK_P[a][d] == BLANK_P[d][s]) && (BLANK_P[a][d] !== num)) {
+						BLANK_P[d][s] = num;
+					}
+				}
+			}
+		}
+	}
+	console.log(BLANK_P);
+}
+
+
+RandomNambers();
+
+
+//цикл 
+//Усовие при нажатии на BLANK[4][3](key12) удалить key12 и запонить его kye0 
+//С Взаимной заменой их свойств  
 
